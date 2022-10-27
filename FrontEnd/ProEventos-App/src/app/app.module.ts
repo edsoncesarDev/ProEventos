e61673905921726ce7b6660587ac36b36b211ce2
edson <edson.dev10@gmail.com>
2022-10-27 19:40:40 -0300

    Criando tabela

--- FrontEnd/ProEventos-App/src/app/app.module.ts
+++ FrontEnd/ProEventos-App/src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //importando BrowserAnimationsModule
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //importando
 
-import { HttpClientModule } from '@angular/common/http' //importando HttpClientModule
+import { HttpClientModule } from '@angular/common/http' //importando
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse'; // importado
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
-  ],
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule, //importando HttpClientModule
-    BrowserAnimationsModule //importando BrowserAnimationsModule para o funcionamento do ngxBootstrap
+    HttpClientModule, //importando para fazer requisições no backEnd
+    BrowserAnimationsModule, //importando para o funcionamento do ngxBootstrap
+    CollapseModule.forRoot() //importado para utilizar Toggle collapse no component nav
   ],
   providers: [],
   bootstrap: [AppComponent]
